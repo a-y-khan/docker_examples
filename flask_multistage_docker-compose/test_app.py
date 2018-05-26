7c522a533cfcc37c8e9c34557727fc0c37e4bda0
Ayla Khan <ayla.khan@gmail.com>
2018-05-26 00:03:39 -0600

    Clean up and do check for some content in db route.

--- flask_multistage_docker-compose/test_app.py
+++ flask_multistage_docker-compose/test_app.py
@@ -8,27 +8,22 @@ from flask_app import create_app
 def test_client():
     flask_app = create_app()
     flask_app.debug = True
-    # return app.test_client()
     testing_client = flask_app.test_client()
-    print(testing_client)
     ctx = flask_app.app_context()
     ctx.push()
 
     yield testing_client
 
     ctx.pop()
-    # return flask_app
 
 
 def test_hello_world(test_client):
     res = test_client.get('/')
-    print(res.status_code)
     assert res.status_code == 200
     assert b'Hello, World!' in res.data
 
 
 def test_db(test_client):
     res = test_client.get('/db')
-    print(res.status_code)
     assert res.status_code == 200
-    # TODO: check data
+    assert b'Mongo Server Information' in res.data
